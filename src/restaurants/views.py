3cd5336f2de6030ef5aee595d922531b70b8ba19
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-11-25 15:26:33 +0100

    template init

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello")
-    #return render(request,"home.html",{})
+    num = random.randint(0, 1000)
+    ls = [num, random.randint(0, 1000), random.randint(0, 1000)]
+    context = {"var": True, "bool_item": True, "num": num, "list": ls}
+    return render(request, "base.html", context)
